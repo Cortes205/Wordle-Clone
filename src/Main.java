c2308c2efec0976762d24f64815cdac9719eae07
Alan <112457025+Cortes205@users.noreply.github.com>
2022-10-23 22:10:25 -0400

    main class

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+// Worldle Game - Recreated
+// Date: 10/24/22
+
+
+import java.awt.*;
+import java.io.*;
+
+public class Main 
+{
+
+	public static void main(String[] args) throws IOException, FontFormatException 
+	{
+		new Wordle();
+	}
+
+}
